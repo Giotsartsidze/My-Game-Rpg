d54d87ad9ce7a24b9db27ec69cc08736fd600dc2
giorgi tsartsidze <tsartsidze30@gmail.com>
2023-11-29 23:30:54 +0400

    latest changes

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Entity
+{   
+    [SerializeField]protected LayerMask whatIsPlayer;
+    [Header("Move Info")]
+    public float moveSpeed;
+    public float idleTime;
+
+    [Header("Attack Info")]
+    public float attackDistance;
+   
+    public EnemyStateMachine stateMachine {get; private set;}
+    
+    protected override void Awake() {
+        base.Awake();
+        stateMachine = new EnemyStateMachine();
+    }
+    protected override void Update() {
+        base.Update();
+        stateMachine.currentState.Update();
+    }
+
+    public virtual RaycastHit2D IsPlayerDetected() {
+    // Check for termination condition
+    // ...
+
+    // Make the raycast call
+    return Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, 10, whatIsPlayer);
+}
+    protected override void OnDrawGizmos() {
+        base.OnDrawGizmos();
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + attackDistance  * facingDir, transform.position.y ));
+    }
+}
---
+++ Assets/Scripts/EnemyState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState 
+{
+   protected EnemyStateMachine stateMachine;
+   protected Enemy enemyBase;
+   protected Rigidbody2D rb;
+
+   private string animBoolname;
+   protected float stateTimer;
+   protected bool triggerCalled;
+
+   public EnemyState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName){
+    this.enemyBase = _enemyBase;
+    this.stateMachine = _stateMachine;
+    this.animBoolname = _animBoolName;
+   }
+
+   public virtual void Enter(){
+    triggerCalled = false;
+    rb = enemyBase.rb;  
+    enemyBase.anim.SetBool(animBoolname, true);
+   }
+
+   public virtual void Update(){
+    stateTimer -= Time.deltaTime;
+   }
+
+   public virtual void Exit(){
+    enemyBase.anim.SetBool(animBoolname, false);
+
+   }
+}
